7b5f86015cd52e249232cec7ab3a8f2180ce61cc
Romario M <mojicaromario@gmail.com>
2022-04-08 18:30:18 -0500

    test show img

---
+++ src/utils/viewImg.js
@@ -0,0 +1,18 @@
+
+export const showImg = () => {
+    const imgToShow = document.querySelector('.itemShop');
+    const bodyDocument = document.querySelector('body');
+
+    
+    bodyDocument.style = 'background-color: rgba(0,0,0, 0.8)';
+
+    
+    
+    /* imgToShow.style.transform = "scale(1.5)"
+    
+    imgToShow.style = 'margin: 0 auto;' ;
+
+    imgToShow.style.transition = "transform 0.25s ease" */
+
+    console.log('click');
+};
