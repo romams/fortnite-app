b3f8b49bc19525922490d773abb93cc40bd342b3
Romario M <mojicaromario@gmail.com>
2022-04-14 13:48:10 -0500

    Se creo el componente CurrentMap

---
+++ src/components/home/currentMap/currentMap.js
@@ -0,0 +1,12 @@
+
+const CurrentMap = ({currentMap}) => {
+
+    return <div className='row'>
+        <div className='col-md-12 col-sm-12'>
+            <p className='current-map-section-title'>Mapa del Capitulo 3 Temporada 2</p>
+            <img src={currentMap} alt="current Map" className="curent-map" />
+        </div>
+    </div>
+}
+
+export default CurrentMap;
