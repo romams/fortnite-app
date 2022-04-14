722a729a8576271dc59123f5d8c67228d623bdea
Romario M <mojicaromario@gmail.com>
2022-04-14 13:47:56 -0500

    Se creo el compontent del crew

---
+++ src/components/home/crew/ftCrew.js
@@ -0,0 +1,37 @@
+
+const FtCrew = ({ftCrew, ftCrewRewards}) => {
+
+    return <div className='row'>
+        <p className='ft-crew-title'>Club fortnite</p>
+
+        <div className='col-md-7 col-sm-12'>
+            <div className="card">
+                <img src={ftCrew?.images?.apiRender} className="card-img-top" alt="..." />
+            </div>
+        </div>
+        <div className='col-md-5 col-sm-12'>
+
+            <div className='crew-rewards-content'>
+                {
+                    ftCrewRewards?.map((reward) => {
+                        return <div className="card reward-card-img text-white" key={reward?.item?.id}>
+                            <img
+                                src={reward?.item?.images?.background}
+                                className="card-img crew-reward-img"
+                                alt="..." />
+
+                            <div className="card-img-overlay">
+                                <h6 className="card-title">{reward?.item?.name}</h6>
+                                <p className="card-text">{reward?.item?.description}</p>
+                            </div>
+                        </div>
+                    })
+                }
+
+            </div>
+        </div>
+    </div>
+
+}
+
+export default FtCrew;
