9ec1f428efe840db147300d384340bb9814d46af
Romario M <mojicaromario@gmail.com>
2022-04-14 14:10:37 -0500

    Se agrego icon de github en el footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,7 @@
 import './footer.css';
 import { Link } from 'wouter';
+import {GoMarkGithub} from 'react-icons/go'
+import {AiFillHeart} from 'react-icons/ai'
 
 function Footer() {
     return <div className="footer mt-5">
@@ -9,27 +11,41 @@ function Footer() {
                     <h4 className="footer-heading">FtTracker</h4>
                 </div>
             </div>
-            <div className='col-md4 col-sm-12'>
-                <div className='menu-footer'>
-                    <h5 className='quick-menu'>Quick Menu</h5> 
-                    <ul>
-                        <li>
-                            <Link href={`${process.env.PUBLIC_URL}/`} className="nav-link">Home</Link>
-                        </li>
-                        <li>
-                            <Link href={`${process.env.PUBLIC_URL}/shop`} className="nav-link">Item shop</Link>
-                        </li>
-                        <li>
-                            <Link href={`${process.env.PUBLIC_URL}/news`} className="nav-link">news</Link>
-                        </li>
-                        <li>
-                            <Link href={`${process.env.PUBLIC_URL}/stats`} className="nav-link">stats</Link>
-                        </li>
-                        <li>
-                            <Link href={`${process.env.PUBLIC_URL}/battlepass`} className="nav-link">Battle Pass</Link>
-                        </li>
-                    </ul>
+            <div className='row'>
+                
+                <div className='col-md-7 col-sm-5 col-6'>
+                    <div className='menu-footer'>
+                        <h5 className='quick-menu'>Quick Menu</h5>
+                        <ul>
+                            <li>
+                                <Link href={`${process.env.PUBLIC_URL}/`} className="nav-link">Inicio</Link>
+                            </li>
+                            <li>
+                                <Link href={`${process.env.PUBLIC_URL}/shop`} className="nav-link">Tienda</Link>
+                            </li>
+                            <li>
+                                <Link href={`${process.env.PUBLIC_URL}/news`} className="nav-link">Noticias</Link>
+                            </li>
+                            <li>
+                                <Link href={`${process.env.PUBLIC_URL}/stats`} className="nav-link">Estadisticas</Link>
+                            </li>
+                            <li>
+                                <Link href={`${process.env.PUBLIC_URL}/battlepass`} className="nav-link">Pase de batalla</Link>
+                            </li>
+                        </ul>
+                    </div>
+
+
                 </div>
+
+                <div className='col-md-5 col-sm-7 col-6'>
+                    <div className='github-credits'>
+                        <p>Alojado con <AiFillHeart /> en </p>
+                        <GoMarkGithub  className='github-icon'/>
+                    </div>
+                </div>
+
+
             </div>
             <div className="row mt-3">
                 <div className="col-md-12 col-sm-12 copyright">
