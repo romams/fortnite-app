76b3d8178b48ef89bb0cfeedec0a83ced9915e36
Romario M <mojicaromario@gmail.com>
2022-04-17 20:29:28 -0500

    Se creo el componente tienda para el inicio

---
+++ src/components/home/itemShop/itemShop.js
@@ -0,0 +1,32 @@
+import { useDailyShop } from "hooks/useDailyShop";
+import { Link } from "wouter";
+
+
+const ItemShop = () => {
+    const {itemsShop} = useDailyShop();
+
+    return <div>
+        {
+            Object.keys(itemsShop).slice(0,2).map((section, idx)=> {
+                return <div key={idx}>
+                    
+                    <p className="section-title shop-title-section">{section}</p>
+
+                    <div className="itemshop-section">    
+                        {
+                            itemsShop[section]
+                                .slice(0,7)
+                                .map((item, idx) => <img src={item} alt="..." key={idx}/>)
+                        }
+                    </div>
+                </div>
+            })
+        }
+
+        <div className="view-items-shop section-title">
+            <Link href={`${process.env.PUBLIC_URL}/shop`}>Ver tienda completa</Link>
+        </div>
+    </div>
+}
+
+export default ItemShop;
