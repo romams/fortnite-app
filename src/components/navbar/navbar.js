19e4cd47467ff2438066eabd3db87afca00a0248
Romario M <mojicaromario@gmail.com>
2022-04-17 20:27:05 -0500

    Se cambio el color de la navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   
   return <>
     <header id="header">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <nav className="navbar navbar-expand-lg navbar-dark">
             <div className="container-fluid">
                 <a className="navbar-brand navbar-title" href="#">FtTracker</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
