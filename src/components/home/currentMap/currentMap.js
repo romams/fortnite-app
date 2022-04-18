9ab6140fe2357eaeee56545c969eb4ff23a57b74
Romario M <mojicaromario@gmail.com>
2022-04-17 20:26:40 -0500

    Se modifico el titulo

--- src/components/home/currentMap/currentMap.js
+++ src/components/home/currentMap/currentMap.js
@@ -3,7 +3,7 @@ const CurrentMap = ({currentMap}) => {
 
     return <div className='row'>
         <div className='col-md-12 col-sm-12'>
-            <p className='section-title'>Mapa del Capitulo 3 Temporada 2</p>
+            <p className='section-title current-map-title'>Mapa del Capitulo 3 Temporada 2</p>
             <img src={currentMap} alt="current Map" className="curent-map" />
         </div>
     </div>
