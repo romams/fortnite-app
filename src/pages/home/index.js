548db9975fbf6adde75e19f8358784a834b39911
Romario M <mojicaromario@gmail.com>
2022-04-17 20:28:54 -0500

    Se cambio el carrusel port la tienda

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,24 +1,35 @@
 import './home.css';
-import {useFortniteNews} from 'hooks/useFortniteNews';
 import {useFtCrew} from 'hooks/useFtCrew';
 import { useCurrentMap } from 'hooks/useCurrentMap';
-import Carousel from 'components/home/carousel/carousel';
 import FtCrew from 'components/home/crew/ftCrew';
 import CurrentMap from 'components/home/currentMap/currentMap';
+import NewsSection from 'components/news/newsSection';
+import ItemShop from 'components/home/itemShop/itemShop';
 
 function Home() {
-
-    const {ftNews} = useFortniteNews();
     const {ftCrew, ftCrewRewards} = useFtCrew();
     const {currentMap} = useCurrentMap();
     
     return <>
+        <img 
+            src='https://cdn2.unrealengine.com/fortnite-chapter-3-season-2-1900x600-c561fe397af1.jpg'
+            className='header-img'
+            alt='season-bg' />
 
-        <Carousel ftNews={ftNews} />
-        
-        <FtCrew ftCrew={ftCrew} ftCrewRewards={ftCrewRewards}/>
+        <div className='container'>
+            <NewsSection />
+            
+            <FtCrew ftCrew={ftCrew} ftCrewRewards={ftCrewRewards}/>
 
-        <CurrentMap currentMap={currentMap} />
+            <div className='row'>
+                <div className='col-lg-8 col-md-12'>
+                    <CurrentMap currentMap={currentMap} /> 
+                </div>
+                <div className='col-lg-4 col-md-12'>
+                    <ItemShop />
+                </div>
+            </div>
+        </div>
         
     </>
 }
