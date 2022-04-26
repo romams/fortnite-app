f1f5ab44c6663b8843ce0805c8a4099648e84af9
Romario M <mojicaromario@gmail.com>
2022-04-26 17:50:17 -0500

    Obtiene los items y los ordena

---
+++ src/hooks/useItems.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+import { getItems } from "services/itemsService"
+
+export const useItems = (type) => {
+
+    const [cosmetics, setCosmetics] = useState([]);
+    
+    if(type === '') type = 'outfit';
+    
+    useEffect(() => {
+
+            getItems(type.toLowerCase()).then(({data}) => sortItemsByAddedDate(data))
+    },[type]);
+
+
+    const sortItemsByAddedDate = (items) =>{
+        items.sort(function(a,b){
+            if(a.added < b.added){
+                return 1;
+            }
+
+            if(a.added > b.added){
+                return -1;
+            }
+
+            return 0;
+        })
+
+        setCosmetics(items)
+    }
+
+
+    return {cosmetics}
+}
