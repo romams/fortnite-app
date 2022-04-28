157b94402f9df841c370d4cc22faa5d9f0317cbf
Romario M <mojicaromario@gmail.com>
2022-04-27 20:34:19 -0500

    Se agrego el fondo a los items y el evento onclick

--- src/components/items/cosmetic/cosmetic.js
+++ src/components/items/cosmetic/cosmetic.js
@@ -1,27 +1,46 @@
+import useItemSelected from 'hooks/useItemSelected';
+import epic from 'img/rarity/epic.jpg';
+import legendary from 'img/rarity/legendary.jpg';
+import rare from 'img/rarity/rare.jpg';
+import uncommon from 'img/rarity/uncommon.jpg';
+import { useEffect } from 'react';
 import './cosmetic.css'
 
 
-const Cosmetic = ({cosmetics}) => {
+const Cosmetic = ({ cosmetics }) => {
+
+    const { onSelectItem } = useItemSelected();
+    const st ={epic, legendary, rare, uncommon}
+    
+    useEffect(() => {
+
+        const divimg = document.querySelectorAll('.item-icon-img');
+
+        for(let i =0; i <= divimg.length-1; i++ ){
+            divimg[i].style.backgroundImage = `url(${st[divimg[i]?.id]})`
+        }
+    }, [cosmetics])
 
     return <>
-        {   cosmetics?.length > 0 && (
-            cosmetics?.map(item => <div className='item-icon' key={item.id}>
-                
-                <div className='item-icon-img plus'>
-                    <img src={item?.images?.icon} alt="" className="img-item"/>
-                </div>
-                {/* <div className='item-icon-name'>
-                    <span>{item?.name}</span>
-                </div> */}
-            </div>
-                
-             )
 
+        {
+            cosmetics?.length > 0 && (
+                cosmetics?.map(item => <div className='' key={item.id}>
+                    <div className='item-icon-img plus'id={item?.rarity?.value}>
+                        <img
+                            src={item?.images?.icon}
+                            className="img-item"
+                            onClick={(() => onSelectItem(item))}
+                            alt=""
+                        />
+                    </div>
+                </div>
+                )
             )
         }
     </>
 
-    
+
 }
 
 export default Cosmetic;
